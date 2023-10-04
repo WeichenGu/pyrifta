289a2073bd67e1060bd2efc2bb8f0572c386418e
Weichen Gu <weichen.gu@diamond.ac.uk>
2023-10-04 11:07:01 +0100

    add entensions

--- lib_rifta/surface_extension_2d/__init__.py
+++ lib_rifta/surface_extension_2d/__init__.py
@@ -3,5 +3,9 @@ from .Surface_Extension_Zero import *
 from .Surface_Extension_Gauss import *
 from .Surface_Extension_8NN import *
 from .Surface_Extension_Fall import *
-
-
+from .Surface_Extension_GP import *
+# from .Surface_Extension_Smooth import *
+from .Surface_Extension_GerchbergPapoulis import *
+from .Surface_Extension_Iter import *
+from .Surface_Extension_Iter_freq import *
+from .frequency_separate_dct import *
