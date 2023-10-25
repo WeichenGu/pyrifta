2035a00c07d3a701444754d67ce89947cca94504
Weichen Gu <weichen.gu@diamond.ac.uk>
2023-10-25 11:32:22 +0100

    add BRFSuperGaussian2D

--- lib_rifta/__init__.py
+++ lib_rifta/__init__.py
@@ -1,4 +1,5 @@
 from .BRFGaussian2D import BRFGaussian2D
+from .BRFSuperGaussian2D import BRFSuperGaussian2D 
 from .Surface_Extension import Surface_Extension
 from . import surface_extension_2d
 from .DwellTime2D_FFT_Full_Test import DwellTime2D_FFT_Full_Test
